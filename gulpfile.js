4d071a5ef868018a5048296d8b5653a67bef893b
Keven Jesus <falecom@keven.com.br>
2017-05-22 17:07:11 -0300

    plugin gulp sass para compilar o scss, deixar apenas um watch por demanda

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require("gulp");
+var sass = require("gulp-sass");
 var htmlmin = require('gulp-htmlmin');
 var cleanCSS = require('gulp-clean-css');
 
@@ -10,11 +11,13 @@ gulp.task('minify-html', function() {
 
 gulp.task('minify-css', function() {
   return gulp.src('./source/scss/*.scss')
+  	.pipe(sass())
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('./dist/css'));
 });
 	
 
 gulp.task('monitorar', function(){
-	gulp.watch('./source/**/*.*',['minify-html','minify-css']);
+	gulp.watch('./source/scss/*.scss',['minify-css']);
+	gulp.watch('./source/index.html',['minify-html']);
 });
